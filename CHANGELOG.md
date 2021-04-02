9ca1aa4c13485da2fcab1f9baedb4ec6bf8886a0
DeStiCap <53054030+DeStiCap@users.noreply.github.com>
2021-04-02 21:19:24 +0700

    V0.0.0-preview.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,2 @@
-## [0.0.0-preview.1] - 2021-04-02
+## [0.0.0-preview.2] - 2021-04-02
 - Add Extension_Tilemap script.
--- Scripts/Extensions/Extension_Tilemap.cs
+++ Scripts/Extensions/Extension_Tilemap.cs
@@ -13,7 +13,7 @@ namespace DSC.Tilemap2D
         /// </summary>
         /// <param name="hTilemap">Tilemap</param>
         /// <returns>Tile cell position</returns>
-        public static Vector3Int GetTileCellPositionAtCursor(this Tilemap hTilemap)
+        public static Vector3Int CellAtCursor(this Tilemap hTilemap)
         {
             return hTilemap.WorldToCell(InputUtility.GetCursorWorldPosition());
         }
@@ -23,9 +23,20 @@ namespace DSC.Tilemap2D
         /// </summary>
         /// <param name="hTilemap">Tilemap</param>
         /// <returns>Tile world position center</returns>
-        public static Vector3 GetTileWorldPositionCenterAtCursor(this Tilemap hTilemap)
+        public static Vector3 GetCellCenterWorldAtCursor(this Tilemap hTilemap)
         {
-            return hTilemap.GetCellCenterWorld(hTilemap.GetTileCellPositionAtCursor());
+            return hTilemap.GetCellCenterWorld(hTilemap.CellAtCursor());
+        }
+
+        /// <summary>
+        /// Get tile world position center at this position.
+        /// </summary>
+        /// <param name="hTilemap">Tilemap</param>
+        /// <param name="vPos">World Position</param>
+        /// <returns>Tile world position center</returns>
+        public static Vector3 GetCellCenterWorldAtPosition(this Tilemap hTilemap,Vector3 vPos)
+        {
+            return hTilemap.GetCellCenterWorld(hTilemap.WorldToCell(vPos));
         }
     }
 }
